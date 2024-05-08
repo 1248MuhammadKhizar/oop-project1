78af793be861e358d7eaa9beffcebb177e31b5b7
1248MuhammadKhizar <mkhizar124839@gmail.com>
2024-05-08 22:26:56 +0500

    added post class and some functions

--- project.cpp
+++ project.cpp
@@ -138,6 +138,26 @@ User_()
 
 }
 
+string GetUserName(const string& userId) const {
+	for (int i = 0; i < numOfUser; ++i) {
+		if (user_id[i] == userId) {
+			return User[i][0] + " " + User[i][1];
+		}
+	}
+	return "User not found";
+}
+
+bool IsUser(const string& a) const 
+	{
+		for (int i = 0; i < numOfUser; ++i) 
+		{
+			if (user_id[i] == a) 
+			{
+				return true; 
+			}
+		}
+		return false; 
+	}
 
 void LoadData(const string& Text)
 {
@@ -210,6 +230,11 @@ void SetCurrentUser(const string & a)
 	}
 }
 
+string get_current_user()
+{
+	return CurrentUser ;
+}
+
 void ViewFriendsOfCurrentUser()
 {
 	for (int i = 0; i < numOfUser; i++) 
@@ -269,19 +294,223 @@ void ViewLikedPagesOfCurrentUser(const PageSet& pages)
 };
 
 
-
-
-
-
-int main()
+class Post
 {
-	User_ u1;
-	PageSet p1;
-	u1.LoadData("user.txt");
-	u1.SetCurrentUser("u7");
-	u1.ViewFriendsOfCurrentUser();
-	p1.LoadData("Page.txt");
-	u1.ViewLikedPagesOfCurrentUser(p1);
+	private:
 
+	int num_of_posts;
+	int * likes;
+	string * id;
+	string * owner;
+	string * content;
+	string * date;
+	string like[12][12];
+
+public:
+Post()
+{
+	ReadData();
+}
+
+
+
+void ReadData() 
+{
+	ifstream file("Post.txt");
+	if (!file.is_open()) {
+		cerr << "Unable to open file." << endl;
+		return;
+	}
+	file >> num_of_posts;
+	likes = new int[num_of_posts];
+	id = new string[num_of_posts];
+	date = new string[num_of_posts];
+	content = new string[num_of_posts];
+	owner = new string[num_of_posts];
+	for (int i = 0; i < num_of_posts; ++i)
+	{
+		file >> likes[i];
+		file >> id[i];
+		file >> date[i];
+		file.ignore(); 
+		getline(file, content[i]);
+		getline(file, owner[i]);
+		int j = 0;
+		while (true) {
+			file >> like[i][j];
+			if (like[i][j] == "-1" || like[i][j].empty()) break;
+			++j;
+		}
+	}
+	file.close();
+
+}
+
+void like_post(User_ & a,string id1)
+{
+	for(int i=0;i<num_of_posts;i++)
+	{
+		string current_id=a.get_current_user();
+		if(id[i]==id1)
+		{
+			bool liked=false;
+			for (int j = 0; j < 10; ++j)
+			{
+				if (like[i][j] == current_id){
+					liked = true;
+					break;
+				}
+			}
+			if (liked)
+			{
+				cout << "You have already liked this post." << endl;
+			}
+			else if (likes[i] < 10)
+			{
+				like[i][likes[i]] = current_id;
+				++likes[i];
+				cout << " liked the post." << endl;
+			}
+			else
+			{
+				cout << "Maximum number of likes reached for this post." << endl;
+			}
+			break;
+		}
+	}
+	 
+}
+
+
+void view_people_liked(string & p_id,User_ & a,PageSet& pges)
+{
+bool found=false;
+for(int i=0;i<num_of_posts;i++)
+{
+if(id[i]==p_id)
+{
+
+	found=true;
+	cout<<"the post is  "<<id[i]<<endl;
+	cout<<content[i]<<" "<<endl;
+	cout<<"post owner is "<<owner[i]<<endl;
+	cout<<"the post is liked by "<<endl;
+
+    for(int j=0;like[i][j]!="-1";j++)
+     {
+           string likr=like[i][j];
+            if (a.IsUser(likr))
+				{
+					cout << a.GetUserName(likr)<<" ";
+				}
+				else
+				{
+					cout << pges.GetPageName(likr)<<" "; 
+				}
+				cout<<endl;
+				break;
+   }
+}
+}
+
+
+
+if(!found)
+{
+	cout<<"post not found  "<<endl;
+}
+
+
+}
+
+
+};
+
+
+
+
+
+int main(){
+User_ u1;
+	PageSet p1;
+	Post p2;
+
+	u1.LoadData("Text.txt");
+	p1.LoadData("page.txt");
+	string user;
+	cout << " Enter ID : ";
+	cin >> user;
+	bool found = false;
+	do
+	{
+		if (user == "u1" || user == "u2" || user == "u3" || user == "u4" || user == "u5" || user == "u6" || user == "u7" || user == "u8" || user == "u9" || user == "u10" || user == "u11" || user == "u12" || user == "u13" || user == "u14" || user == "u15" || user == "u16" || user == "u17" || user == "u18" || user == "u19" || user == "u20")
+		{
+			found = true;
+			u1.SetCurrentUser(user);
+
+		}
+		else
+		{
+			
+			cout << " No user Occur with ID :" << user << "\n";
+			cout << " Enter ID : ";
+			cin >> user;
+
+		}
+
+
+	} while (found==false);
 	
+	int opt;
+	do
+	{
+		cout << "Menu:\n";
+		cout << "1. View Friends of Current User :\n";
+		cout << "2. View Liked People of a Post :\n";
+		cout << "3. Like a Post :\n";
+		cout << "4. Set User : \n";
+		cout << "0. Exit\n";
+		cout << "Enter your choice: ";
+		cin >> opt;
+		if (opt == 1)
+		{
+			u1.ViewFriendsOfCurrentUser();
+
+		}
+		else if (opt == 2)
+		{
+			string postId;
+			cout << " Enter Post ID : ";
+			cin >> postId;
+			p2.view_people_liked(postId, u1, p1);
+
+		}
+		else if (opt == 3)
+		{
+			string postId;
+			cout << " Enter Post ID : ";
+			cin >> postId;
+			p2.like_post(u1, postId);
+
+		}
+		else if (opt == 4)
+		{
+			string user;
+			cout << " Enter ID : ";
+			cin >> user;
+			if (user == "u1" || user == "u2" || user == "u3" || user == "u4" || user == "u5" || user == "u6" || user == "u7" || user == "u8" || user == "u9" || user == "u10" || user == "u11" || user == "u12" || user == "u13" || user == "u14" || user == "u15" || user == "u16" || user == "u17" || user == "u18" || user == "u19" || user == "u20")
+			{
+				u1.SetCurrentUser(user);
+
+			}
+
+			else
+			{
+				cout << " No user Occur with ID :" << user << "\n";
+
+			}
+		}
+		
+	} while (opt!=0);
+
 }
